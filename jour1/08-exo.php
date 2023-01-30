882f99d225219de797cd2d06aba49a483aabc223
Aziz S <aziz.soltani2001@hotmail.com>
2023-01-30 14:39:01 +0100

    correction ex 8

--- jour1/08-exo.php
+++ jour1/08-exo.php
@@ -4,10 +4,14 @@ $prenom = "Victor";
 $nom = "Hugo";
 $ville = "Paris";
 
-$phrase = "$prenom $nom a vécu à $ville en 1800. <br>";
+$phrase1 = "$prenom $nom a vécu à $ville en 1800. <br>";
 $phrase2 = "$prenom a quitté $ville à la fin de sa carrière <br>";
 $phrase3 = "$prenom $nom a écrit \"Notre Dame de Paris\"" ;
 
-echo $phrase;
+echo $phrase1;
 echo $phrase2;
 echo $phrase3;
+
+echo "$phrase1 $phrase2 $phrase3"
+
+// http://localhost/php-initiation/jour1/08-exo.php
